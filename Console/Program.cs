36280461efb7293a534e7731ae37ca47bcf074b6
Alexey Kuptsov <alxkuptsov@gmail.com>
2023-05-07 20:10:23 +0300

    ++

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,16 +1,17 @@
 ﻿var notes = new[] { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
 var scales = new[] { "Aeolian", "Locrian", "Ionian", "Dorian", "Phrygian", "Lydian", "Mixolydian" };
 
-Console.WriteLine($"{notes[Random.Shared.Next(notes.Length)]} {scales[Random.Shared.Next(scales.Length)]}");
-Console.WriteLine($"{notes[Random.Shared.Next(notes.Length)]} {scales[Random.Shared.Next(scales.Length)]}");
+var random = Random.Shared;
+Console.WriteLine($"{notes[random.Next(notes.Length)]} {scales[random.Next(scales.Length)]}");
 
-Console.WriteLine($"{notes[(Random.Shared.Next(3) + 7) % 12]}m");
-Console.WriteLine($"{notes[(Random.Shared.Next(3) + 7) % 12]}m");
+Console.WriteLine($"{notes[(random.Next(8) + 4) % 12]}{(random.Next(4) == 0 ? "" : "m")}");
+Console.WriteLine($"{notes[(random.Next(8) + 4) % 12]}{(random.Next(4) == 0 ? "" : "m")}");
+Console.WriteLine($"{notes[(random.Next(8) + 4) % 12]}{(random.Next(4) == 0 ? "" : "m")}");
 
-Console.WriteLine(Random.Shared.Next(3) != 0 ? 
-    $"{Random.Shared.Next(16) + 1}, {Random.Shared.Next(16) + 1}" :
-    $"{Random.Shared.Next(8) + 17}, {Random.Shared.Next(8) + 17}");
-Console.WriteLine($"{Random.Shared.Next(24) + 1}, {Random.Shared.Next(24) + 1}");
-var tt1 = Random.Shared.Next(24) + 1;
-var tt2 = Random.Shared.Next(24) + 1;
+Console.WriteLine(random.Next(3) != 0 ? 
+    $"{random.Next(16) + 1}, {random.Next(16) + 1}" :
+    $"{random.Next(8) + 17}, {random.Next(8) + 17}");
+Console.WriteLine($"{random.Next(24) + 1}, {random.Next(24) + 1}");
+var tt1 = random.Next(24) + 1;
+var tt2 = random.Next(24) + 1;
 Console.WriteLine($"{tt1}, {tt1}, {tt2}, {tt2}");
